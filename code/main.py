3325090d3c3c15d44e37440670a59ad4901ee13c
ido volf <ido96647@gmail.com>
2025-07-05 22:03:06 +0300

    comments

--- code/main.py
+++ code/main.py
@@ -1,17 +1,24 @@
 import pygame
+from reso import resource_path
+from mnst import Monster
 pygame.init()
 
+#stting vars
 winSize = (640,380)
 window = pygame.display.set_mode(winSize)
 clock = pygame.time.Clock()
 run  = True
 
-while run:
+#loading shit
+bg = pygame.image.load(resource_path("assets/battle ui/battleBox.png"))
 
-    for event in pygame.event.get():
+while run:
+    window.blit(bg,bg.get_rect()) #drawing bg
+
+    for event in pygame.event.get(): #basic event handling
         if(event.type == pygame.QUIT):
             run = False
 
 
-    pygame.display.update()
+    pygame.display.update() #updating screen each frame
     clock.tick(60)
