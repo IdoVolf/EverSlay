a77752b14cf74e8e4c3b865a40f5da095384b24c
ido volf <ido96647@gmail.com>
2025-07-21 18:52:59 +0300

    fixed death on full screen not aviable to exit bug

--- code/death.py
+++ code/death.py
@@ -19,6 +19,8 @@ def Death(window,player):
         for event in pygame.event.get():
             if(event.type == pygame.QUIT):
                 return "menu"
+            if(event.type == pygame.KEYDOWN):
+                return "menu"
             
         
         if(txt.collidepoint(mousePos) and mousePressed[0]):
